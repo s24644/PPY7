6a006df20e683270c69bc48f81f50ade54e54c7d
s24644 <s24644@pjwstk.edu.pl>
2023-05-19 23:05:42 +0200

    Dodanie klienta poprzez nowe okno, poprawione usuwanie studenta

--- PPY7/zadanie.py
+++ PPY7/zadanie.py
@@ -25,20 +25,49 @@ def load_data():
         treeview.insert("", "end", values=(row[0], row[1], row[2], row[3], row[4]))
 
 
-def add_student():  # Dopiero później zobaczyłem że może chodzić o otwarcie nowego okna, edycja studenta już będzie tak zrealizowana :)
-    text_data = add_student_entry.get()
-    splitted = text_data.split(",")
-    query = f"INSERT INTO studenci VALUES ({int(splitted[0])}, '{splitted[1]}', '{splitted[2]}', " \
-            f"{float(splitted[3])}, {float(splitted[4])})"
-    conn.execute(query)
-    load_data()
+def open_new_student_window():
+    new_window = tk.Toplevel(root)
+    new_window.title("Dodaj nowego studenta")
 
+    id_label = ttk.Label(new_window, text="ID:")
+    id_label.pack()
+    id_entry = ttk.Entry(new_window)
+    id_entry.pack()
 
-def delete_student():
-    id_to_delete = int(delete_student_entry.get())
-    query = f"DELETE FROM studenci WHERE id = {id_to_delete}"
-    conn.execute(query)
-    load_data()
+    first_name_label = ttk.Label(new_window, text="Imie:")
+    first_name_label.pack()
+    first_name_entry = ttk.Entry(new_window)
+    first_name_entry.pack()
+
+    last_name_label = ttk.Label(new_window, text="Nazwisko:")
+    last_name_label.pack()
+    last_name_entry = ttk.Entry(new_window)
+    last_name_entry.pack()
+
+    points_label = ttk.Label(new_window, text="Punkty:")
+    points_label.pack()
+    points_entry = ttk.Entry(new_window)
+    points_entry.pack()
+
+    grade_label = ttk.Label(new_window, text="Ocena:")
+    grade_label.pack()
+    grade_entry = ttk.Entry(new_window)
+    grade_entry.pack()
+
+    def add_new():
+        new_id = id_entry.get()
+        new_first_name = first_name_entry.get()
+        new_last_name = last_name_entry.get()
+        new_points = points_entry.get()
+        new_grade = grade_entry.get()
+        query = f"INSERT INTO studenci VALUES ({new_id}, '{new_first_name}', '{new_last_name}', " \
+                f"{float(new_points)}, {float(new_grade)})"
+        conn.execute(query)
+        load_data()
+        new_window.destroy()
+
+    add_button = ttk.Button(new_window, text="Dodaj", command=add_new)
+    add_button.pack()
 
 
 def open_details_window(event):
@@ -87,8 +116,16 @@ def open_details_window(event):
             conn.execute(query)
             load_data()
 
+        def delete_student():
+            query = f"DELETE FROM studenci WHERE ID = {id_entry.get()}"
+            conn.execute(query)
+            load_data()
+
         alter_button = tk.Button(details_window, text="Zmien dane studenta", command=alter_student)
         alter_button.pack()
+
+        delete_button = tk.Button(details_window, text="Usun studenta", command=delete_student)
+        delete_button.pack()
 
 
 create_table_query = '''CREATE TABLE IF NOT EXISTS studenci (
@@ -106,23 +143,8 @@ left_frame = tk.Frame(root, borderwidth=4, relief="ridge", width=int(root.winfo_
 left_frame.pack(side="left", padx=10, pady=10)
 left_frame.pack_propagate(0)
 
-add_student_label = tk.Label(left_frame, text="Aby dodać studenta, podaj jego dane oddzielone ','")
-add_student_label.pack(padx=10, pady=10)
-
-add_student_entry = tk.Entry(left_frame)
-add_student_entry.pack(anchor="n", padx=10, pady=10)
-
-add_student_button = tk.Button(left_frame, text="Wprowadź dane", command=add_student)
+add_student_button = tk.Button(left_frame, text="Dodaj studenta", command=open_new_student_window)
 add_student_button.pack(anchor="center", padx=10, pady=10)
-
-delete_student_label = tk.Label(left_frame, text="Aby usunąć studenta, podaj jego id")
-delete_student_label.pack(padx=10, pady=10)
-
-delete_student_entry = tk.Entry(left_frame)
-delete_student_entry.pack(anchor="n", padx=10, pady=10)
-
-delete_student_button = tk.Button(left_frame, text="Usun studenta", command=delete_student)
-delete_student_button.pack(anchor="center", padx=10, pady=10)
 
 right_frame = tk.Frame(root, width=int(root.winfo_width() * 5), height=left_frame["height"], borderwidth=4,
                        relief="ridge")
@@ -142,10 +164,11 @@ treeview.bind("<Double-1>", open_details_window)
 
 conn.execute('''INSERT INTO studenci VALUES (1, 'Jan', 'Kowalski', '66', '3.5')''')
 
-show_data_button = tk.Button(right_frame, text="Wyświetl dane w tabeli", command=load_data)
+show_data_button = tk.Button(left_frame, text="Wyświetl dane w tabeli", command=load_data)
 show_data_button.pack(anchor="center", padx=10, pady=10)
 
-alter_student_label = tk.Label(right_frame, text="Aby zmienić dane studenta, kliknij na niego podwójnie na tabeli",
+alter_student_label = tk.Label(right_frame, text="Aby zmienić dane lub usunąć studenta, "
+                                                 "kliknij na niego podwójnie na tabeli",
                                font=("Arial", 10))
 alter_student_label.pack(padx=10, pady=10)
 
